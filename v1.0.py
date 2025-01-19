43f28837f8cda5627e26f96b04701676d985eacc
Mohammed Aman <73650271+amxn167@users.noreply.github.com>
2025-01-19 19:13:45 +0400

    Update v3.0.py

--- v1.0.py
+++ v1.0.py
@@ -3,6 +3,7 @@ import requests
 import json
 import pandas as pd
 import re
+import urllib.parse
 
 st.markdown(
     """
@@ -18,13 +19,13 @@ st.markdown(
 )
 
 # VirusTotal API Key
-VT_API_KEY = st.secrets["vtkey"]
+VT_API_KEY = "b208107450f8af1b55f735fe4377820a4b6baef21d5734c383a099e3271796ee"
 
 # AbuseIPDB API Key
-ABUSEIPDB_API_KEY = st.secrets["aipdbkey"]
+ABUSEIPDB_API_KEY = "a81598f36599471d707ce45964fa0eaddd91127e934f7dbb52b33d829adc0e9982b3ce83334d3546"
 
 # Hybrid-Analysis API Key
-HYBRID_API_KEY = st.secrets["hybridkey"]
+HYBRID_API_KEY = "353qykml83fafbd44gyra3nlc99268b99euha3880b9f6801e41fgho52db60e64"
 
 
 # Function to get VirusTotal report
@@ -47,6 +48,8 @@ def get_abuseipdb_report(ip_address):
     try:
         response = requests.get(url, headers=headers, params=params)
         response.raise_for_status()  # Raise an exception for bad status codes
+        # with open('abuse_report.txt', 'w') as f:
+        #     json.dump(response.json(), f, indent=4)
         return response.json()
     except requests.exceptions.RequestException as e:
         print(f"Error fetching AbuseIPDB report: {e}")
@@ -62,6 +65,7 @@ def get_whois_report(ip_address):
     except requests.exceptions.RequestException as e:
         print(f"Error fetching Who.is report: {e}")
         return None
+
 
 # Function to get Hybrid-Analysis report
 def get_hybrid_report(hash_value):
@@ -85,22 +89,38 @@ def get_hybrid_report(hash_value):
     
 # Function to get VirusTotal report for hash
 def get_vt_report_hash(hash_value):
-    url = "https://www.virustotal.com/vtapi/v2/file/report"
-    params = {"apikey": VT_API_KEY, "resource": hash_value}
+    url = f"https://www.virustotal.com/api/v3/files/{hash_value}"
+    headers = {"accept": "application/json", "x-apikey": VT_API_KEY}
     try:
-        response = requests.get(url, params=params)
+        response = requests.get(url, headers=headers)
         response.raise_for_status()  
-        with open('vt_report.txt', 'w') as f:
-            json.dump(response.json(), f, indent=4)
+        # with open('vt_report.txt', 'w') as f:
+        #     json.dump(response.json(), f, indent=4)
         return response.json()
     except requests.exceptions.RequestException as e:
         print(f"Error fetching VirusTotal report for hash: {e}")
         return None
+    
+# Function to get VirusTotal report for domain
+def get_vt_report_domain(domain):
+    url = f"https://www.virustotal.com/api/v3/domains/{domain}"
+    headers = {"accept": "application/json", "x-apikey": VT_API_KEY}
+    try:
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()
+        # with open('vt_domainreport.txt', 'w') as f:
+        #     json.dump(response.json(), f, indent=4)
+        return response.json()
+        return response.json()
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching VirusTotal report for domain: {e}")
+        return None
+
 
 # Streamlit interface
 st.title("Bulk IP Address and Hash Checker")
 
-tab_ip, tab_hash, tab_about = st.tabs(["IP Address", "Hash", "About"])
+tab_ip, tab_hash, tab_domain, tab_about = st.tabs(["IP Address", "Hash", "Domain", "About"])
 
 with tab_ip:
     upload_file = st.file_uploader("Upload a txt file with IPs separated by newlines", type=["txt"])
@@ -226,27 +246,27 @@ Hybrid-Analysis: 100 requests per day">API limitations</a>
             hash_values_list = hash_values
         else:
             hash_values_list = [hash_value.strip() for hash_value in hash_values.split(",")]
+        
         data = []
         for hash_value in hash_values_list:
             vt_report = None
             hybrid_report = None
+            
             if "VirusTotal" in reports:
                 vt_report = get_vt_report_hash(hash_value)
+            
             if "Hybrid-Analysis" in reports:
                 hybrid_report = get_hybrid_report(hash_value)
             row = {
                 "Hash Value": hash_value,
                 "VirusTotal": {
-                    "Scan ID": vt_report.get("scan_id", "") if vt_report else "",
-                    "SHA256": vt_report.get("sha256", "") if vt_report else "",
-                    "MD5": vt_report.get("md5", "") if vt_report else "",
-                    "Link": vt_report.get("permalink", "") if vt_report else "",
-                    "Malicious": vt_report.get("positives","") if vt_report else "",
-                    "Detected Vendors": vt_report.get("detected_vendors", 0) if vt_report else 0,
-                    "Detected URLs": len(vt_report.get("detected_urls", [])) if vt_report else 0,
-                    "Verbose Message": vt_report.get("verbose_msg", "") if vt_report else "",
-                    "Stats": vt_report.get("stats", {}) if vt_report else {},
-                    "Total": vt_report.get("total", "") if vt_report else {}
+                    "SHA1": vt_report.get("data", {}).get("attributes", {}).get("sha1", 0) if vt_report else "",
+                    "Malicious": vt_report.get("data", {}).get("attributes", {}).get("last_analysis_stats", {}).get("malicious", 0) if vt_report else "No matches found!",
+                    "Suspicious": vt_report.get("data", {}).get("attributes", {}).get("last_analysis_stats", {}).get("suspicious", 0) if vt_report else "",
+                    "Undetected": vt_report.get("data", {}).get("attributes", {}).get("last_analysis_stats", {}).get("undetected", 0) if vt_report else "",
+                    "Harmless": vt_report.get("data", {}).get("attributes", {}).get("last_analysis_stats", {}).get("harmless", 0) if vt_report else "",
+                    "Tags": vt_report.get("data", {}).get("attributes", {}).get("meaningful_name", 0) if vt_report else "No matches found!",
+                    "Popular Threat Classification": vt_report.get("data", {}).get("attributes", {}).get("popular_threat_classification", {}).get("suggested_threat_label", "") if vt_report else "No matches found!"
                 },
                 "Hybrid-Analysis": {
                     "Threat Score": hybrid_report[0].get("threat_score", 0) if hybrid_report else 0,
@@ -262,33 +282,92 @@ Hybrid-Analysis: 100 requests per day">API limitations</a>
                     "Detected URLs": len(hybrid_report[0].get("detected_urls", [])) if hybrid_report else 0,
                     "Stats": hybrid_report[0].get("stats", {}) if hybrid_report else {}
                 }
-
             }
             data.append(row)
+        
         if "VirusTotal" in reports:
-            df_vt = pd.DataFrame([{"Hash Value": d["Hash Value"], 
-                                   "Score": f"{d['VirusTotal']['Malicious']}/{d['VirusTotal']['Total']}",
-                                   "Verbose Message": d["VirusTotal"]["Verbose Message"], 
-                                   "Link": d["VirusTotal"]["Link"]
-                                   } for d in data])
+            df_vt = pd.DataFrame([{
+                "SHA1": d["Hash Value"],
+                "Malicious Rating": f"{d['VirusTotal']['Malicious']}/{d['VirusTotal']['Malicious'] + d['VirusTotal']['Suspicious'] + d['VirusTotal']['Undetected'] + d['VirusTotal']['Harmless']}" if d['VirusTotal']['Malicious'] != 0 else "No matches found",
+                "Tags": d["VirusTotal"]["Tags"],
+                "Popular Threat Name": d["VirusTotal"]["Popular Threat Classification"],
+                "Link": f"https://www.virustotal.com/gui/file/{d['Hash Value']}"
+            } for d in data])
+            
             st.write("VirusTotal Report:")
             st.write(df_vt)
+        
         if "Hybrid-Analysis" in reports:
-            df_hybrid = pd.DataFrame([{"Hash Value": d["Hash Value"], 
-                                      "Threat Level": d["Hybrid-Analysis"].get("Threat Level", "") if "Hybrid-Analysis" in d else "",
-                                      "Verdict": d["Hybrid-Analysis"].get("Verdict", "") if "Hybrid-Analysis" in d else "",
-                                      "AV Detect": f"{d['Hybrid-Analysis'].get('AV Detect', 0)}/100" if "Hybrid-Analysis" in d else 0,
-                                      "VX Family": d["Hybrid-Analysis"].get("VX Family", "") if "Hybrid-Analysis" in d else "",
-                                      "Link": f"https://www.hybrid-analysis.com/sample/{d['Hash Value']}"
-                                      } for d in data])
+            df_hybrid = pd.DataFrame([{
+                "Hash Value": d["Hash Value"], 
+                "Threat Level": d["Hybrid-Analysis"].get("Threat Level", "") if "Hybrid-Analysis" in d else "",
+                "Verdict": d["Hybrid-Analysis"].get("Verdict", "") if "Hybrid-Analysis" in d else "",
+                "AV Detect": f"{d['Hybrid-Analysis'].get('AV Detect', 0)}/100" if "Hybrid-Analysis" in d else 0,
+                "VX Family": d["Hybrid-Analysis"].get("VX Family", "") if "Hybrid-Analysis" in d else "",
+                "Link": f"https://www.hybrid-analysis.com/sample/{d['Hash Value']}"
+            } for d in data])
+            
             st.write("Hybrid-Analysis Report:")
             st.write(df_hybrid)
+with tab_domain:
+    upload_file = st.file_uploader("Upload a txt file with domains separated by newlines", type=["txt"])
+    if upload_file:
+        domains = upload_file.read().decode("utf-8").splitlines()
+    else:
+        domains = st.text_input("Enter domains separated by commas:")
+
+    reports = st.multiselect("Select Reports", ["VirusTotal"], default=["VirusTotal"])
+    st.write("Please refrain from using VirusTotal if more than 4 Domains involved!")
+
+    st.markdown("""
+    <a href="#" data-toggle="tooltip" title="API Limitations: 
+    VirusTotal: 4 requests per minute">API limitations</a>
+    """, unsafe_allow_html=True)
+
+    if st.button("Check Domain", key="check_domain"):
+        if upload_file:
+            domains_list = domains
+        else:
+            domains_list = [domain.strip() for domain in domains.split(",")]
+
+        data = []
+        for domain in domains_list:
+            vt_report = None
+            if "VirusTotal" in reports:
+                vt_report = get_vt_report_domain(domain)
+            row = {
+                "Domain": domain,
+                "VirusTotal": {
+                    "Malicious": vt_report.get("data", {}).get("attributes", {}).get("last_analysis_stats", {}).get("malicious", 0) if vt_report else "",
+                    "Suspicious": vt_report.get("data", {}).get("attributes", {}).get("last_analysis_stats", {}).get("suspicious", 0) if vt_report else "",
+                    "Undetected": vt_report.get("data", {}).get("attributes", {}).get("last_analysis_stats", {}).get("undetected", 0) if vt_report else "",
+                    "Harmless": vt_report.get("data", {}).get("attributes", {}).get("last_analysis_stats", {}).get("harmless", 0) if vt_report else "",
+                }
+            }
+            data.append(row)
+
+        if "VirusTotal" in reports:
+            df_vt = pd.DataFrame([{
+                "Domain": d["Domain"],
+                "Malicious": f"{d['VirusTotal']['Malicious']}/94",
+                "Undetected": d["VirusTotal"]["Undetected"],
+                "Harmless": d["VirusTotal"]["Harmless"],
+                "Suspicious": d["VirusTotal"]["Suspicious"],
+                "Link": f"https://www.virustotal.com/gui/domain/{d['Domain']}"
+            } for d in data if d["Domain"] is not None and d["Domain"] != ""])
+            st.write("VirusTotal Report:")
+            st.write(df_vt)
+
 with tab_about:
     st.markdown("""
-<a href="#" data-toggle="tooltip" title="Change Log:
-Last Update: 06/10/2024
-Upcoming Updates: Any.Run, URLhaus, MISP, CAPE, Malshare, Valhalla, Hashlookup API Integration for File hashes">ChangeLog</a>
+<a href="#" data-toggle="tooltip" title="Upcoming:
+Upcoming Updates: Any.Run, URLhaus, MISP, CAPE, Malshare, Valhalla, Hashlookup">ChangeLog</a>
 """, unsafe_allow_html=True)
+    st.title("ChangeLog:")
+    st.subheader("19/01/2025:")
+    st.write("1. Change VirusTotal API from v2 to v3 for hashes")
+    st.write("2. Improve table ranges for VirusTotal Hashes")
+    st.write("3. Redundancy exfil across APIs")
     st.title("About:")
     st.write("This tool is designed to help you bulk check IP addresses and hashes for potential security threats.")
     st.write("It uses APIs from VirusTotal, AbuseIPDB, Who.Is and Hybrid-Analysis to gather information about the IP addresses and hashes you enter.")
@@ -307,4 +386,4 @@ Upcoming Updates: Any.Run, URLhaus, MISP, CAPE, Malshare, Valhalla, Hashlookup A
     st.write("4. Who.is API: No API limitations specified, but please refrain from making excessive requests.")
 
     st.subheader("Contact:")
-    st.write("If you have any ideas, questions or concerns, please feel free to contact Mohammed Aman(+971507543199)")
+    st.write("If you have any ideas, questions or concerns, please feel free to contact Mohammed Aman")
